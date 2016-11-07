22eaa1cd1fe60acfb783a861bdbb260cbeee9de7
David Barajas <david@netlandish.com>
2016-11-07 17:10:07 -0600

    Add table with employees that does not come.

--- timetracker/app/controllers/timetracker_controller.rb
+++ timetracker/app/controllers/timetracker_controller.rb
@@ -13,6 +13,15 @@ class TimetrackerController < ApplicationController
 
       # employees that get late
       @late_employees = Timetrack.where("date between ? and ? and inout_type = 'in' and time > '09:00:00'::time", @s, @e).order("date", "time").all
+
+      # employees that does not come
+      d = Date.parse(@s)
+      # TODO: make it a big query instead of many queries.
+      while d <= Date.parse(@e)
+        es = Employee.where("id not in (select b.employee_id from timetracks tt inner join badges b on b.id = tt.badge_id where date = ?)", d).order('name')
+        @dates_not_come << [d, es]
+        d += 1.days
+      end
     end
   end
 end
