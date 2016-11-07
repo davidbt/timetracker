39979d1399ba5f2949e3de5a5c6028c27248f6c7
David Barajas <david@netlandish.com>
2016-11-07 16:33:58 -0600

    Add a table with the late arrivals.

--- timetracker/app/controllers/timetracker_controller.rb
+++ timetracker/app/controllers/timetracker_controller.rb
@@ -1,5 +1,18 @@
 class TimetrackerController < ApplicationController
   before_action :authenticate_user!
   def late
+    @s = ''
+    @e = ''
+    @late_employees = Timetrack.where("time > '09:00:00'::time")
+    # and array or arrays with the dates and the employees that didn't come
+    @dates_not_come = []
+    if params[:range]
+      range = params[:range]
+      @s = range[:start]
+      @e = range[:end]
+
+      # employees that get late
+      @late_employees = Timetrack.where("date between ? and ? and time > '09:00:00'::time", @s, @e).order("date", "time").all
+    end
   end
 end
